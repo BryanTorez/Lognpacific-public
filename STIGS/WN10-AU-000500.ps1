12cc506e1786a8223aff5b008668a3a3259d5698
Bryan Torres <odaglednayrb@gmail.com>
2025-01-09 14:31:58 -0800

    Update WN10-AU-000500.ps1

--- STIGS/WN10-AU-000500.ps1
+++ STIGS/WN10-AU-000500.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+    Camera access from the lock screen must be disabled.
 
 .NOTES
     Author          : Bryan Torres
@@ -11,7 +11,7 @@
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
-    STIG-ID         : WN10-AU-000500
+    STIG-ID         : WN10-CC-000005
 
 .TESTED ON
     Date(s) Tested  : 
@@ -22,23 +22,19 @@
 .USAGE
     Put any usage instructions here.
     Example syntax:
-    PS C:\> .\STIG-ID-WN10-AU-000500.ps1 
+    PS C:\> .\STIG-ID-WN10-CC-000005.ps1 
 #>
 
-# Define the registry key path
-$RegistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application"
+# Path to the registry key
+$RegPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization"
 
-# Define the registry value name and data
-$ValueName = "MaxSize"
-$ValueData = 0x8000 # Hexadecimal value: 00008000
-
-# Check if the registry path exists, if not, create it
-if (-not (Test-Path $RegistryPath)) {
-    New-Item -Path $RegistryPath -Force | Out-Null
+# Check if the registry key exists, create it if not
+if (-not (Test-Path $RegPath)) {
+    New-Item -Path $RegPath -Force
 }
 
-# Set the registry value
-Set-ItemProperty -Path $RegistryPath -Name $ValueName -Value $ValueData
+# Set the "NoLockScreenCamera" value
+Set-ItemProperty -Path $RegPath -Name "NoLockScreenCamera" -Value 1 -PropertyType DWord
 
-Write-Output "Registry value set successfully."
+#>
 
